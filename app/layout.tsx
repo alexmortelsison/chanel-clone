e3c11006e204767a0e548cf9921ccc0ca4f74823
alexmortelsison <alexmortelsison@gmail.com>
2025-10-20 11:22:04 +0900

    imported font couture

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
+
+const couture = localFont({
+  src: "./public/fonts/couture-bld.woff",
+  variable: "--font-couture",
+  weight: "400",
+  style: "normal",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${couture.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
